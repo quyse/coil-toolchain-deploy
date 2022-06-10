2792dee3198086025bfb7d86f46c70eb5e1b5b14
Alexander Bich <code@quyse.net>
2022-06-10 07:25:33 +0000

    add nginxSslConfig

--- cloudflare/default.nix
+++ cloudflare/default.nix
@@ -10,4 +10,14 @@ rec {
   originCertEcc = pkgs.fetchurl {
     inherit (fixeds.fetchurl."https://developers.cloudflare.com/ssl/static/origin_ca_ecc_root.pem") url sha256 name;
   };
+
+  nginxSslConfig = origin: ''
+    ssl_certificate     /certs/${origin}.cloudflare.crt;
+    ssl_certificate_key /certs/${origin}.cloudflare.key;
+    ssl_protocols       TLSv1.3;
+    ssl_ciphers         HIGH:!aNULL:!MD5;
+    ssl_session_tickets on;
+    ssl_client_certificate ${originPullCACert};
+    ssl_verify_client   on;
+  '';
 }
