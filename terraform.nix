de46672202cc38ec44993a2480a944b0973d1c87
Alexander Bich <code@quyse.net>
2024-10-05 15:01:16 +0500

    use opentofu instead of terraform

--- terraform.nix
+++ terraform.nix
@@ -23,13 +23,13 @@ rec {
   , pluginsFun ? (standardPlugins: [])
   , stateDir ? "./state"
   }: let
-    terraform = pkgs.terraform.withPlugins pluginsFun;
+    opentofu = pkgs.opentofu.withPlugins pluginsFun;
     stateDirEscaped = lib.escapeShellArg stateDir;
   in pkgs.writeShellScript "tool" ''
     set -eu
     ln -sf ${moduleFile} ${stateDirEscaped}/main.tf.json
     rm -rf ${stateDirEscaped}/.terraform*
-    ${terraform}/bin/terraform -chdir=${stateDirEscaped} init
-    exec ${terraform}/bin/terraform -chdir=${stateDirEscaped} "$@"
+    ${opentofu}/bin/tofu -chdir=${stateDirEscaped} init
+    exec ${opentofu}/bin/tofu -chdir=${stateDirEscaped} "$@"
   '';
 }
